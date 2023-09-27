087e8593665eeeab551d7f3c59cdeeda391590c4
Utsav <utsav.patel1512@outlook.com>
2023-09-27 22:56:28 +0530

    basic structure 18

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+
+
+import './App.css'; 
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App;
